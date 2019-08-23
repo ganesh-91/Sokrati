425876af8841ba6d757170d9ca0538c2ea43ab00
ganesh.khaire@knorex.com <ganesh.khaire@knorex.com>
2019-08-23 11:39:37 +0530

    Updates

---
+++ precache-manifest.314d590b9a150604fead6647e8e535f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f478df8222a914283b2d90ae3628c01",
+    "url": "/reciepe-grocery-list/index.html"
+  },
+  {
+    "revision": "91cb4f74c18026c7dfbc",
+    "url": "/reciepe-grocery-list/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e6e7946734c7508c77d9",
+    "url": "/reciepe-grocery-list/static/css/main.3638f282.chunk.css"
+  },
+  {
+    "revision": "91cb4f74c18026c7dfbc",
+    "url": "/reciepe-grocery-list/static/js/2.41299da1.chunk.js"
+  },
+  {
+    "revision": "e6e7946734c7508c77d9",
+    "url": "/reciepe-grocery-list/static/js/main.6178d1ed.chunk.js"
+  },
+  {
+    "revision": "ed465b124ff84a324314",
+    "url": "/reciepe-grocery-list/static/js/runtime~main.2ce38d09.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60d18d073bd4cc0ad4ef1be8fe5f88d6.js"
+  "/reciepe-grocery-list/precache-manifest.314d590b9a150604fead6647e8e535f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reciepe-grocery-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
